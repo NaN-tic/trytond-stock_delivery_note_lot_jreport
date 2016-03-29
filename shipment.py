220d3401c25a0dd803c7d5206f85e002c6eda7f1
jmartin <jmartin@zikzakmedia.com>
2016-03-29 12:00:44 +0200

    Add python 3 support

--- shipment.py
+++ shipment.py
@@ -5,8 +5,8 @@ from trytond.pool import PoolMeta
 from trytond.modules.jasper_reports.jasper import JasperReport
 
 __all__ = ['DeliveryNote']
-__metaclass__ = PoolMeta
 
 
 class DeliveryNote(JasperReport):
+    __metaclass__ = PoolMeta
     __name__ = 'stock.shipment.out.delivery_note'
